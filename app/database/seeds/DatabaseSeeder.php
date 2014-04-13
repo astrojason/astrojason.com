b4cb1173503bacacda3856959e1acb0076eaf861
Jason Sylvester <jason.sylvester@hyfn.com>
2014-04-12 21:35:01 -0700

    Create my user account

--- app/database/seeds/DatabaseSeeder.php
+++ app/database/seeds/DatabaseSeeder.php
@@ -2,16 +2,33 @@
 
 class DatabaseSeeder extends Seeder {
 
-	/**
-	 * Run the database seeds.
-	 *
-	 * @return void
-	 */
-	public function run()
-	{
-		Eloquent::unguard();
+  /**
+   * Run the database seeds.
+   *
+   * @return void
+   */
+  public function run()
+  {
+    Eloquent::unguard();
+    $this->call('UserTableSeeder');
 
-		// $this->call('UserTableSeeder');
-	}
+    $this->command->info('User table seeded!');
+  }
+
+}
+
+class UserTableSeeder extends Seeder {
+
+  public function run()
+  {
+    DB::table('users')->delete();
+
+    User::create(array(
+      'name'     => 'Jason Sylvester',
+      'username' => 'astrojason',
+      'email'    => 'jason@astrojason.com',
+      'password' => Hash::make('awesome'),
+    ));
+  }
 
 }
