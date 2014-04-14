5a2f1826caa32804678d76a15e033f736acf08b4
Jason Sylvester <jason.sylvester@hyfn.com>
2014-04-13 20:06:39 -0700

    Allow books to have null goodreads_id

---
+++ app/database/migrations/2014_04_14_002106_update_books_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateBooksTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+    DB::update("ALTER TABLE books ALTER COLUMN goodreads_id DROP NOT NULL");
+    DB::update("ALTER TABLE books ALTER COLUMN author_lname DROP NOT NULL");
+    DB::update("ALTER TABLE books ALTER COLUMN author_lname DROP NOT NULL");
+    DB::update("ALTER TABLE books ALTER COLUMN series DROP NOT NULL");
+    DB::update("ALTER TABLE books ALTER COLUMN seriesorder DEFAULT 0");
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
