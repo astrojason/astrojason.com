dba45d9708382f814eb900f8ad56f838ddae26b6
Jason Sylvester <jason.sylvester@hyfn.com>
2014-04-23 18:24:49 -0700

    Fixing the games table

---
+++ app/database/migrations/2014_04_24_012025_update_games_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateGamesTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+    Schema::create('games', function($table)
+    {
+      $table->increments('id');
+      $table->integer('user_id');
+      $table->string('title');
+      $table->string('platform');
+      $table->boolean('completed');
+      $table->timestamps();
+    });
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+    Schema::drop('games');
+	}
+
+}
