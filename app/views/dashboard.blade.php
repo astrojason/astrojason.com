49a7eb7a04791646514af3de60415acffc3f6e37
Jason Sylvester <jason.sylvester@hyfn.com>
2014-10-22 10:15:58 -0700

    Fixed issue with hockey exercise not updating

--- app/views/dashboard.blade.php
+++ app/views/dashboard.blade.php
@@ -20,7 +20,7 @@
         <div class="panel panel-default" ng-show="linkCtrl.hockey.length > 0">
           <div class="panel-heading clearfix">
             <h3 class="panel-title pull-left">Hockey Exercise</h3>
-            <div class="glyphicon glyphicon-refresh link-action pull-right" ng-click="linkCtrl.refreshCategory('Hockey', 3)"></div>
+            <div class="glyphicon glyphicon-refresh link-action pull-right" ng-click="linkCtrl.refreshCategory('Hockey Exercise', 3)"></div>
           </div>
           <table class="table table-striped">
             <tr ng-repeat="link in linkCtrl.hockey" ng-include src="'js/templates/linkInfo.html'"></tr>
