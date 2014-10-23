a1dfeed6510fce5c8120d0237c8392fb0e91c033
Jason Sylvester <jason.sylvester@hyfn.com>
2014-10-23 06:29:28 -0700

    Show the all links page even when there are no links

--- app/views/links.blade.php
+++ app/views/links.blade.php
@@ -3,15 +3,15 @@
 @section('content')
   <div ng-controller="allLinksController as linkCtrl">
     <div class="row">
-      <div class="col-md-12 clearfix page-controls" ng-show="linkCtrl.links">
-        <span class="pull-left" ng-show="filtered">@{{ filtered.length }} total links</span>
+      <div class="col-md-12 clearfix page-controls">
+        <span class="pull-left" ng-show="linkCtrl.links">@{{ filtered.length }} total links</span>
         <span class="pull-right"><input type="text" class="form-control" ng-model="filter" /></span>
       </div>
     </div>
     <div class="row">
       <div class="col-md-12">
         <table class="table table-striped all-links">
-          <tr ng-repeat="link in filtered = (linkCtrl.links | filter:filter)" ng-class="link.read ? 'read' : ''" ng-include src="'js/templates/linkInfo.html'"></tr>
+          <tr ng-repeat="link in linkCtrl.links" ng-class="link.read ? 'read' : ''" ng-include src="'js/templates/linkInfo.html'"></tr>
         </table>
       </div>
     </div>
