09d09ba03a3b202d170874f7657143ae07d54695
Jason Sylvester <jason.sylvester@hyfn.com>
2015-04-10 06:50:28 -0700

    Added phpDoc to Game and Song models

--- app/models/Game.php
+++ app/models/Game.php
@@ -1,5 +1,23 @@
-<?
+<?php
 
+/**
+ * Game
+ *
+ * @property integer $id 
+ * @property integer $user_id 
+ * @property string $title 
+ * @property string $platform 
+ * @property boolean $completed 
+ * @property \Carbon\Carbon $created_at 
+ * @property \Carbon\Carbon $updated_at 
+ * @method static \Illuminate\Database\Query\Builder|\Game whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\Game whereUserId($value)
+ * @method static \Illuminate\Database\Query\Builder|\Game whereTitle($value)
+ * @method static \Illuminate\Database\Query\Builder|\Game wherePlatform($value)
+ * @method static \Illuminate\Database\Query\Builder|\Game whereCompleted($value)
+ * @method static \Illuminate\Database\Query\Builder|\Game whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\Game whereUpdatedAt($value)
+ */
 class Game extends Eloquent {
 
   public function __construct() {
--- app/models/Song.php
+++ app/models/Song.php
@@ -1,6 +1,26 @@
 <?php
 
 
+/**
+ * Song
+ *
+ * @property integer $id 
+ * @property integer $user_id 
+ * @property string $title 
+ * @property string $artist 
+ * @property string $location 
+ * @property boolean $learned 
+ * @property \Carbon\Carbon $created_at 
+ * @property \Carbon\Carbon $updated_at 
+ * @method static \Illuminate\Database\Query\Builder|\Song whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\Song whereUserId($value)
+ * @method static \Illuminate\Database\Query\Builder|\Song whereTitle($value)
+ * @method static \Illuminate\Database\Query\Builder|\Song whereArtist($value)
+ * @method static \Illuminate\Database\Query\Builder|\Song whereLocation($value)
+ * @method static \Illuminate\Database\Query\Builder|\Song whereLearned($value)
+ * @method static \Illuminate\Database\Query\Builder|\Song whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\Song whereUpdatedAt($value)
+ */
 class Song extends Eloquent {
 
   public function __construct() {
