c202fe2c389485fc3440192c33f9eaa2baef8f58
Jason Sylvester <jason@astrojason.com>
2016-03-25 12:14:52 -0700

    Make the js files an include to prevent future failures to include necessary files

--- app/views/readlater.blade.php
+++ app/views/readlater.blade.php
@@ -27,23 +27,5 @@
         ng-show="!error && editing">
       </link-form>
     </div>
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
-    <script type="text/javascript" src="assets/bower/jquery/dist/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
-    <script type="text/javascript" src="assets/bower/angular/angular.min.js"></script>
-    <script type="text/javascript" src="assets/bower/angular-resource/angular-resource.min.js"></script>
-    <script type="text/javascript" src="assets/bower/angular-animate/angular-animate.min.js"></script>
-    <script type="text/javascript" src="assets/bower/alertifyjs/dist/js/alertify.js"></script>
-    <script type="text/javascript" src="assets/bower/angular-fx/dist/angular-fx.min.js"></script>
-    <script type="text/javascript" src="assets/bower/angular-messages/angular-messages.min.js"></script>
-    <script type="text/javascript" src="assets/bower/moment/min/moment.min.js"></script>
-    <script type="text/javascript" src="assets/bower/eonasdan-bootstrap-datetimepicker/build/js/bootstrap-datetimepicker.min.js"></script>
-    <script type="text/javascript" src="assets/js/vendor/bootstrap.min.js"></script>
-    <script type="text/javascript" src="assets/js/app.min.js"></script>
-    <script type="text/javascript" src="assets/js/models.min.js"></script>
-    <script type="text/javascript" src="assets/js/directives.min.js"></script>
-    <script type="text/javascript" src="assets/js/filters.min.js"></script>
-    <script type="text/javascript" src="assets/js/services.min.js"></script>
-    <script type="text/javascript" src="assets/js/resources.min.js"></script>
-    <script type="text/javascript" src="assets/js/controllers.min.js"></script>
+    @include('partial.js')
   </body>
