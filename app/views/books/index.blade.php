4217769c9d799472cd2dbfd1536447c62f3b6d18
Jason Sylvester <jason@astrojason.com>
2016-11-16 15:02:46 -0800

    Add clear search input button to books list

--- app/views/books/index.blade.php
+++ app/views/books/index.blade.php
@@ -16,7 +16,13 @@
             </tr>
             <tr>
               <th class="input-group">
-                <input type="text" ng-model="book_query" class="form-control" placeholder="Search Query" />
+                <div class="search-wrapper">
+                  <input type="text" ng-model="book_query" class="form-control" placeholder="Search Query" />
+                  <small
+                    class="clear-input glyphicon glyphicon-remove-circle"
+                    ng-show="book_query"
+                    ng-click="book_query = ''"></small>
+                </div>
                 <div class="input-group-addon"><input type="checkbox" ng-model="is_read" /> <label>Include read</label></div>
               </th>
             </tr>
