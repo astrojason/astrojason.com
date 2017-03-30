3f5b875f870badb169ac7926ab316459ac461171
Jason Sylvester <jason@astrojason.com>
2017-03-29 17:06:34 -0700

    Moved the react components into their own directory

--- public/assets/jsx/App.jsx
+++ public/assets/jsx/App.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 import {render} from 'react-dom';
 import {createStore} from 'redux';
 
-import ArticleList from './Article/ArticleList.jsx'
-import User from './User/User.jsx'
-import Chains from './Habit/Chain.jsx'
-import Tasks from './Task/Tasks.jsx'
-import Practice from './Guitar/Practice.jsx'
-import Songs from './Guitar/Song.jsx'
-import Books from './Book/Book.jsx'
+import ArticleList from './Components/Article/ArticleList.jsx'
+import User from './Components/User/User.jsx'
+import Chains from './Components/Habit/Chain.jsx'
+import Tasks from './Components/Task/Tasks.jsx'
+import Practice from './Components/Guitar/Practice.jsx'
+import Songs from './Components/Guitar/Song.jsx'
+import Books from './Components/Book/Book.jsx'
 
 import articles from './reducers/articles.jsx'
 import user from './reducers/user.jsx'
